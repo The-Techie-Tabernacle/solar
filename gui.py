389fe4a85c1e898367be4b993eb5cc0b6b2d2c2f
The-Muhammad-Musa <126743241+The-Muhammad-Musa@users.noreply.github.com>
2023-05-12 00:14:31 -0400

    GUI Revamp started
    GUI itself got the revamp, functionality not done. Calling this v1.0.1 for now, will fix functionality tomorrow.

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@
 The GUI file for Project Solar! Originally Solar was a console only script however this is being changed as to make the
 script more user-friendly!
 
+Patch Notes vMG-1.0.1: Revamping front end for 1.1 release
 Patch Notes vAX-0.1.2: Hotfixes for existing work
 Patch Notes vMG-0.1.1: Seems to mostly be working, I think most of the issues rn are in the solar module not gui
 Patch Notes vMG-0.1: Actually created the file, current calls from solar itself so still does console printing
@@ -12,16 +13,20 @@ import solar as so
 # Setting the color scheme
 sg.theme("NeutralBlue")
 
-# Create a list for our wonderful dropdown menu
-dropdownList = ["Del Non-Endo", "Non-WA in Region", "Nation Non-Endo", "Debugging"]
+# Create a list for our wonderful dropdown menus
+actionList = ["Non-Endo", "Non-WA", "Deathwatch"]
+regNatList = ["Region", "Nation"]
+tagTGList = ["TAG", "TG"]
 
 # Create the layout
 layout = [
-    [sg.Text("Welcome to Solar!", key="OUT")],
-    [sg.Text("Main Nation: "), sg.Input(key="user-agent")],
-    [sg.Text("Target: "), sg.Input(key="target")],
-    [sg.Text("Desired Action: "), sg.Combo(values=dropdownList, readonly=True, key="action"), sg.Button('TAG', size=(4, 1), button_color='white on black', key='tag')],
-    [sg.Button("Exit", size=(3, 1), key="exit"), sg.Button("Submit", size=(5, 1), key="submit")],
+    [sg.Text("Welcome to Solar!", key="-OUT-")],
+    [sg.Text("Main Nation:     "), sg.Input(size=(40, 5), key="user-agent")],
+    [sg.Text("Desired Target: "), sg.Input(size=(30, 5), key="-TARG-"), sg.Combo(values=regNatList,
+                                                                         readonly=True, key="-REGNAT-")],
+    [sg.Text("Desired Action: "), sg.Combo(values=actionList, readonly=True, key="-ACTION-"),
+     sg.Combo(values=tagTGList, readonly=True, key="-TAGTG-")],
+    [sg.Button("Exit", size=(3, 1), key="-EXIT-"), sg.Button("Submit", size=(5, 1), key="-SUBMIT-")]
 ]
 
 # Create the window!
@@ -42,9 +47,6 @@ def GetHeaders(values):
     return headers
 
 
-# For the TAG/TG button!
-down = graphic_off = True
-
 # Loop to open and hold open the gui
 while event != "exit":
     event, values = window.read()
@@ -56,6 +58,7 @@ while event != "exit":
         break
     elif event == "submit":
         # Wonderful, wonderful match case
+        # TODO - Redo match case for new options
         match (values["action"]):
             case "Del Non-Endo":
                 post = so.region_info(headers, "NER", values["target"])
@@ -75,11 +78,6 @@ while event != "exit":
 
             case other:
                 print("Yeet")
-
-    elif event == 'tag':  # if the normal button that changes color and text
-        down = not down
-        window['tag'].update(text='TAG' if down else 'TG',
-                             button_color='white on black' if down else 'white on black')
     else:
         print("test")
 
